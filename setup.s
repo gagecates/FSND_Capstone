7320d994ba1120f725735a18cffb421e116af9bd
Gage Cates <gagecates10@gmail.com>
2020-12-14 18:30:44 -0700

    setup

---
+++ setup.s
@@ -0,0 +1,2 @@
+DATABASE_URL = 'postgres://znmaldbrgwwcdm:f6e2e61418481e3abceac29bd72443f5beacc963ef0427b553df437062d3b88f@ec2-50-19-247-157.compute-1.amazonaws.com:5432/d43clbblknstvc;
+EXCITED = true 
