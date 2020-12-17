3c180f0418e48a09de377755f81a109be93d36fe
Gage Cates <gagecates10@gmail.com>
2020-12-16 18:27:01 -0700

    Add Auth

--- app.py
+++ app.py
@@ -10,13 +10,20 @@ setup_db(app)
 CORS(app)
 
 # run function below upon initial startup to initialize tables. Delete/comment out after creation. 
-# db_drop_and_create_all()
-
+#db_drop_and_create_all()
 '''
-name = "gage"
-macros = 50
-calories = 200
-new_item = Macros(user = name, macros = macros, calories = calories)
+name = "James"
+my_protein = 50
+my_carbs = 10
+my_fats = 100
+my_calories = 200
+new_item = Macros(
+    user = name, 
+    protein = my_protein, 
+    carbs = my_carbs, 
+    fats = my_fats, 
+    calories = my_calories
+)
 new_item.insert()
 '''
 
@@ -25,38 +32,41 @@ new_item.insert()
 def get_macros():
     user = Macros.query.filter_by(user = "gage").first()
     if not user:
-        abort(404)
+        abort(400)
 
     protein = user.protein
     carbs = user.carbs
     fats = user.fats
-    calories = calories
+    calories = user.calories
 
     
     return jsonify({
         'success': True,
-        'protein': protein
-        'carbs': carbs
-        'fats': fats
+        'protein': protein,
+        'carbs': carbs,
+        'fats': fats,
         'calories': calories
     })
 
 '''
 User submits a food. If the food is not found need an error raised and a way to add to DB.
 If the food is in the DB, its macros and calories get added to the users current macro and calorie count.
-The users Macros are then updated in the DB using a PATCH command.
+The users Macros are then updated in the DB using a POSTcommand.
 
 '''
 
-@app.route('/macros', methods=['PATCH'])
+@app.route('/food', methods=['POST'])
 #@requires_auth('post:food')
 def add_food():
     user = Macros.query.filter_by(user = "gage").first()
     data = request.get_json()
     if 'food' not in data:
-        abort(404)
+        return ({
+            'success': False,
+            'message': "Looks like that food is not saved. You will need to add it."
+        })
     
-    food = Food.query.filter_by(food = data['food'])first()
+    food = Food.query.filter_by(food = data['food'].lower()).first()
 
     user_protein = user.protein
     user_carbs = user.carbs
@@ -74,19 +84,85 @@ def add_food():
     add_calories = user_calories + food_calories
 
     update_user = Macros(
-        protein = add_protein
-        carbs = add_carbs
-        fats = add_fats
+        protein = add_protein,
+        carbs = add_carbs,
+        fats = add_fats,
         calories = add_calories
         )
     update_user.update()
 
+    return jsonify({
+        'success': True,
+        'food': food,
+        'protein': add_protein,
+        'carbs': add_carbs,
+        'fats': add_fats,
+        'calories': add_calories
+    })
+    
 
+@app.route('/food/new', methods=['POST'])
+#@requires_auth('post:new_food')
+def new_food(payload):
+    data = request.get_json()
+    new_food = Food(
+        food = data.food.lower(),
+        protein = data.protein,
+        carbs = data.carbs,
+        fats = data.fats,
+        calories = data.calories
+    )
+
+    new_food.insert()
 
     return jsonify({
         'success': True,
-        'macros': macros
+        'food': new_food,
+        'message': "The food has been added"
     })
+
+
+@app.route('/macros', methods=['POST'])
+#@requires_auth('post:food')
+def add_macros_manually():
+    user = Macros.query.filter_by(user = "gage").first()
+    data = request.get_json()
+    if 'food' not in data:
+        abort(404)
+    
+    user_protein = user.protein
+    user_carbs = user.carbs
+    user_fats = user.fats
+    user_calories = user.calories
+
+    food_protein = data.protein
+    food_carbs = data.carbs
+    food_fats = data.fats
+    food_calories = data.calories
+
+    add_protein = user_protein + food_protein
+    add_carbs = user_carbs + food_carbs
+    add_fats = user_fats + food_fats
+    add_calories = user_calories + food_calories
+
+    update_user = Macros(
+        protein = add_protein,
+        carbs = add_carbs,
+        fats = add_fats,
+        calories = add_calories
+        )
+
+    update_user.update()
+
+    return jsonify({
+        'success': True,
+        'protein': add_protein,
+        'carbs': add_carbs,
+        'fats': add_fats,
+        'calories': add_calories
+    })
+
+
 
 if __name__ == '__main__':
     app.run()
